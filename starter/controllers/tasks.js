75537b076f7c8e7d0cf0bea3d15ec5cae3a6490c
jacobwachtel <jawachtel@gmail.com>
2021-11-05 15:47:29 -0400

    Initial file upload. New project working on serverside

---
+++ starter/controllers/tasks.js
@@ -0,0 +1,34 @@
+const Task = require('../models/app');
+
+const getAllTasks = (req, res) => {
+   res.send('get ALL tasks');
+};
+
+const createTask = async (req, res) => {
+   try {
+      const task = await Task.create(req.body);
+      res.status(201).json({ task });
+   } catch (error) {
+      res.status(500).json({ msg: 'there was an error' });
+   }
+};
+
+const getTask = (req, res) => {
+   res.json({ id: req.params.id });
+};
+
+const updateTask = (req, res) => {
+   res.send('Update task');
+};
+
+const deleteTask = (req, res) => {
+   res.send('Delete task');
+};
+
+module.exports = {
+   getAllTasks,
+   createTask,
+   getTask,
+   updateTask,
+   deleteTask,
+};
