c8513e51c8df3bf7d15ed8ab73678db8bd3a4192
jacobwachtel <jawachtel@gmail.com>
2021-11-08 12:08:03 -0500

    Added some middleware and updated Controller requests

--- starter/app.js
+++ starter/app.js
@@ -3,18 +3,16 @@ const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
+const notFound = require('./middleware/not-found');
 
 //middleware
 app.use(express.static('./public'));
 app.use(express.json());
 
-app.use('/api/v1/tasks', tasks);
+// Routes
 
-// app.get('/api/v1/tasks')                       - get all the tasks
-// app.post('/api/v1/tasks')                      - create a new task
-// app.get('/api/v1/tasks/:id')                   - get single task
-// app.patch('/api/v1/tasks/:id')                 - update task
-// app.delete('/api/v1/tasks/:id')                - delete task
+app.use('/api/v1/tasks', tasks);
+app.use(notFound);
 
 const port = 3000;
 
--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -1,71 +1,61 @@
 const Task = require('../models/Task');
+const asyncWrapper = require('../middleware/async');
 
 // GET ALL TASKS
 
-const getAllTasks = async (req, res) => {
-   try {
-      const tasks = await Task.find({});
-      res.status(200).json({ tasks });
-   } catch (error) {
-      res.status(500).json({ msg: error });
-   }
-};
+const getAllTasks = asyncWrapper(async (req, res) => {
+   const tasks = await Task.find({});
+
+   res.status(200).json({ tasks });
+});
 
 // CREATE NEW TASK
 
-const createTask = async (req, res) => {
-   try {
-      const task = await Task.create(req.body);
-      res.status(201).json({ task });
-   } catch (error) {
-      res.status(500).json({ msg: 'there was an error' });
-   }
-};
+const createTask = asyncWrapper(async (req, res) => {
+   const task = await Task.create(req.body);
+   res.status(201).json({ task });
+});
 
 // GET SINGLE TASK
 
-const getTask = async (req, res) => {
-   try {
-      const { id: taskID } = req.params;
-      const task = await Task.findOne({ _id: taskID });
-      if (!task) {
-         return res.status(404).json({ msg: `no task with id : ${taskID}` });
-      }
-      res.status(200).json({ task });
-   } catch (error) {
-      res.status(500).json({ msg: 'there was an error' });
+const getTask = asyncWrapper(async (req, res) => {
+   const { id: taskID } = req.params;
+   const task = await Task.findOne({ _id: taskID });
+   if (!task) {
+      return res.status(404).json({ msg: `no task with id : ${taskID}` });
    }
-};
+   res.status(200).json({ task });
+});
 
-// UPDATE TASK - POST and PATCH
+// UPDATE TASK
 
-const updateTask = async (req, res) => {
-   try {
-      const { id: taskID } = req.params;
-      const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
-         new: true,
-         runValidators: true,
-      });
-      res.status(200).json({ task });
-   } catch (error) {
-      res.status(500).json({ msg: 'there was an error' });
+const updateTask = asyncWrapper(async (req, res) => {
+   const { id: taskID } = req.params;
+
+   const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+   });
+
+   if (!task) {
+      return res.status(404).json({ msg: `no task with id : ${taskID}` });
    }
-};
+
+   res.status(200).json({ task });
+});
 
 // DELETE TASK
 
-const deleteTask = async (req, res) => {
-   try {
-      const { id: taskID } = req.params;
-      const task = await Task.findOneAndDelete({ _id: taskID });
-      if (!task) {
-         return res.status(404).json({ msg: `no task with id : ${taskID}` });
-      }
-      res.status(200).json({ task });
-   } catch (error) {
-      res.status(500).json({ msg: 'there was an error' });
+const deleteTask = asyncWrapper(async (req, res) => {
+   const { id: taskID } = req.params;
+   const task = await Task.findOneAndDelete({ _id: taskID });
+
+   if (!task) {
+      return res.status(404).json({ msg: `no task with id : ${taskID}` });
    }
-};
+
+   res.status(200).json({ task });
+});
 
 module.exports = {
    getAllTasks,
